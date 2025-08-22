04239b00851b8edfc0243f9f88b80406f08a275f
HP <danhpham.31241028354@st.ueh.edu.vn>
2025-08-22 13:18:40 +0700

    Ex02 (first)

---
+++ Scr/Ex02 (28-08-2025).py
@@ -0,0 +1,9 @@
+# 1. Guess The Number Game:
+# -we will generate a random number with the help of randint() function from 1 to 100 and ask the user to guess it.
+# -After every guess, the user will be told if the number is above or below the randomly generated number.
+# -The user will win if they guess the number maximum five attempts.
+# -Ask the user to stop or continue playing again.
+# ***
+# Add another situations like:
+# level of game (hard (guess 3 times), medium (6 times), easy (10 times)
+# assume that you have 100$, each game you spent 5$. Play game until you choose stop, report the game you win/lost and amount you have.
