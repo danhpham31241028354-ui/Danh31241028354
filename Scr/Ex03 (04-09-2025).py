a6736d6b00529257a420f4ff35698c4b975cc871
HP <danhpham.31241028354@st.ueh.edu.vn>
2025-08-30 11:50:13 +0700

    Ex03 (tt)

--- Scr/Ex03 (04-09-2025).py
+++ Scr/Ex03 (04-09-2025).py
@@ -81,6 +81,50 @@ def ex37():
         if i in C37_2:
             giong_nhau.append(i)
     print("Những phần tử giống nhau giữa hai danh sách là: ",giong_nhau)
+# 39. Write a Python program to convert a list of multiple integers into a single
+# integer.
+# Sample list: [11, 33, 50]
+# Expected Output: 113350
+def ex39():
+    C39 = [11,33,50]
+    print("Số sau khi chuyển từ danh sách là: ",int("".join(str(x) for x in C39)))
+# 41. Write a Python program to create multiple lists.
+def ex41():
+    C41_1 = [1,2,3,4]
+    C41_2 = [5,6,7,8,9,10]
+    C41_3 = ["True", "False", "Not Given"]
+    print("Danh sách 1: ",C41_1)
+    print("Danh sách 2: ",C41_2)
+    print("Danh sách 3: ",C41_3)
+# 42. Write a Python program to find missing (có trong list 1 nhưng không có trong list 2) and additional values (có trong list 2 nhưng không có trong list 1) in two lists.
+# Sample data : Missing values in second list: b,a,c
+# Additional values in second list: g,h
+def ex42():
+    C42_1 = ["a","b","c","d"]
+    C42_2 = ["a","g","h"]
+    co_1_khong_2 = []
+    co_2_khong_1 = []
+    for st in C42_1:
+        if st not in C42_2:
+            co_1_khong_2.append(st)
+    print("Ký tự bị thiếu trong danh sách 1 là: ",co_1_khong_2)
+    for st in C42_2:
+        if st not in C42_1:
+            co_2_khong_1.append(st)
+    print("Ký tự được thêm vào là: ",co_2_khong_1)
+# 43. Write a Python program to split a list into different variables.
+def ex43():
+    C43 = ["Phạm","Thành","Danh"]
+    print("Chữ đầu tiên của danh sách là: ",C43[0])
+    print("Chữ thứ hai của danh sách là: ",C43[1])
+    print("Chữ thứ ba của danh sách là: ",C43[2])
+# 44. Write a Python program to generate groups of five consecutive numbers in a ist.
+def ex44():
+    C44_number = list(range(1,16))
+    C44_group = []
+    for i in range(0,len(C44_number),5):#Quét hết các phần tử từ thứ 0 đến phần tử cuối của danh sách (len(C44_number)) với bước nhảy là 5: 5 số 1 nhóm
+        C44_group_nho = C44_number[i:i+5]
+        C44_group.append(C44_group_nho)
+    print("Danh sách mới sau khi nhóm là: ",C44_group)
 
-
-ex37()
+ex44()
