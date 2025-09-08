913ba8c0bf3cbbf176f568c998eeaf800d2a2d79
HP <danhpham.31241028354@st.ueh.edu.vn>
2025-09-08 09:58:31 +0700

    Ex04 (hoàn thành)

--- Scr/Ex04 (11-09-2025).py
+++ Scr/Ex04 (11-09-2025).py
@@ -45,8 +45,48 @@ if prime_number(n):
 else:
     print(f"Số {n} không là số nguyên tố")
 # 6.Write a Python function to print
-# 1.all prime numbers that less than a number (enter prompt keyboard).
-# 2.the first N prime numbers
-# 7.Write a Python function to check whether a number is "Perfect" or not. Then print all perfect number that less than 1000.
+# a. all prime numbers that less than a number (enter prompt keyboard).
+def prime_less_than_n():
+    prime = []
+    for i in range(2,n):
+        if prime_number(i):
+            prime.append(i)
+    return prime
+n = int(input("Câu 6a - Hãy nhập một số nguyên n: "))
+print(f"Các số nguyên tố nhỏ hơn {n} là: {prime_less_than_n()}")
+# b. the first N prime numbers
+def first_N_prime():
+    first_prime = []
+    count = 0
+    num = 2
+    while count < N:
+        if prime_number(num):
+            first_prime.append(num)
+            count+=1
+        num+=1
+    return first_prime
+N = int(input("Câu 6b - Hãy nhập số lượng số nguyên tố cần in: "))
+# 7.Write a Python function to check whether a number is "Perfect" or not.  Then print all perfect number that less than 1000.
+def so_hoan_hao(n:int):
+    tong_uoc = 0
+    for i in range(1,n):
+        if n % i == 0:
+            tong_uoc += i
+    return tong_uoc == n
+print(so_hoan_hao(6))
+print(so_hoan_hao(1000))
+def so_hoan_hao_be_hon_1000():
+    SHH_be_hon_1000 = []
+    for num in range(2,1000):
+        if so_hoan_hao(num):
+            SHH_be_hon_1000.append(num)
+    return SHH_be_hon_1000
+print(f"Câu 7 - Các số hoàn hảo bé hơn 1000 là: {so_hoan_hao_be_hon_1000()} ")
 # 8.Write a Python function to check whether a string is a pangram or not.
 # (Note : Pangrams are words or sentences containing every letter of the alphabet at least once. For example : "The quick brown fox jumps over the lazy dog"
+import string
+def is_pangram(text):
+    alphabet = set(string.ascii_lowercase) #Lấy tất cả các chữ cái trong bảng chữ cái tiếng Anh
+    letters = set(text.lower()) #Chuyển toàn bộ chuỗi về chữ thường, lấy thành set (bộ ký tự duy nhất)
+    return alphabet.issubset(letters) #Kiểm tra xem alphabet có nằm trong letters không
+print(is_pangram("Câu 8 - The quick brown fox jumps over the lazy dog"))
